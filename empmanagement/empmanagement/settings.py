405bf1a4709f7d58527355a0e736cd668e44b4cc
Vaishnavi9820 <vaishnavichandgude0411@gmail.com>
2025-06-03 12:15:48 +0530

    Fix static files for Render

--- empmanagement/empmanagement/settings.py
+++ empmanagement/empmanagement/settings.py
@@ -152,7 +152,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Media files
 MEDIA_URL = '/media/'
