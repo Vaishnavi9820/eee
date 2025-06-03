dad93da097e7db3a829f2b195b568b9f32491c83
Vaishnavi9820 <vaishnavichandgude0411@gmail.com>
2025-06-03 14:25:06 +0530

    final

--- empmanagement/empmanagement/settings.py
+++ empmanagement/empmanagement/settings.py
@@ -176,15 +176,19 @@ USE_TZ = True
 
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'empmanagement', 'static'),
 ]
 
-# Use WhiteNoise in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional for whitenoise (Render/Heroku)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Media files
